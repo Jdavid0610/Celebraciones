e343f7a37c11b0aee03c6755435ccbb62648dd3e
Julian David Ortiz Alviar <61361287+Jdavid0610@users.noreply.github.com>
2020-12-10 03:58:26 -0500

    alteraciones al js

--- experiment.js
+++ experiment.js
@@ -1,3 +1,5 @@
-var variable_uno="esta es la variable inicial pero despues del primer commit";
+function funcion(){
+    var variable_uno="esta es la variable inicial pero despues del primer commit";
 
-console.log(variable_uno);
+    console.log(variable_uno);
+}
