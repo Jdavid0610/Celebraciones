649c65bc7008189f4e2b188dad8001b34aa68565
Julian David Ortiz Alviar <jdavidortizy2k@gmail.com>
2021-01-14 22:31:16 -0500

    home relativamente terminado con footer y navbar ademas se cambiaron las unidades de medida de px a rem

--- assets/js/experiment.js
+++
@@ -1,5 +0,0 @@
-function funcion(){
-    var variable_uno=document.getElementById("mensaje");
-    variable_uno.innerHTML="ay me precionaste muy duro";
-
-}
---
+++ assets/js/functions.js
@@ -0,0 +1,7 @@
+function screensize(){
+
+    var H=screen.height;
+    var x = H.toString().concat("px");
+    document.body.style.height=x;
+    
+}
